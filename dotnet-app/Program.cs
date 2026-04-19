2bd1e5ab501011079ef15d0036645027d1a65a1b
Colby Prior <colby.prior@octopus.com>
2026-04-20 08:46:22 +1000

    add aws region

--- dotnet-app/Program.cs
+++ dotnet-app/Program.cs
@@ -3,7 +3,7 @@ using Amazon.S3;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var s3 = new AmazonS3Client();
+var s3 = new AmazonS3Client(Amazon.RegionEndpoint.USEast1);
 
 app.MapGet("/", () => new { message = "Hello from .NET demo app!", status = "ok" });
 app.MapGet("/health", () => new { status = "healthy" });
