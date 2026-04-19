01cd18b787dc6169132c3b7de03f1afb5fe0f718
Colby Prior <colby.prior@octopus.com>
2026-04-20 08:16:12 +1000

    fix aws client

--- dotnet-app/Program.cs
+++ dotnet-app/Program.cs
@@ -1,11 +1,12 @@
 using Amazon.S3;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddAWSService<IAmazonS3>();
 var app = builder.Build();
+
+var s3 = new AmazonS3Client();
 
 app.MapGet("/", () => new { message = "Hello from .NET demo app!", status = "ok" });
 app.MapGet("/health", () => new { status = "healthy" });
-app.MapGet("/aws", () => new { sdk = "AWSSDK.S3", service = "s3" });
+app.MapGet("/aws", () => new { sdk = "AWSSDK.S3", version = typeof(AmazonS3Client).Assembly.GetName().Version?.ToString(), service = "s3" });
 
 app.Run();
